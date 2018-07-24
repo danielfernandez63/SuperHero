6c4c5a6ef41f57767f7d7124da04e256c5f0a66f
danielfernandez63 <danmanfernan@gmail.com>
2018-07-24 14:46:34 -0500

    added views and all database references. Need to create actions

---
+++ SuperheroCreator/Controllers/HeroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperheroCreator.Controllers
+{
+    public class HeroController : Controller
+    {
+        // GET: Hero
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
